4c3fdb3a543300467fd82e65563dbe3b428be515
Bikal <bikaladhikari5@gmail.com>
2024-06-30 15:58:39 +1000

    Interface based refactoring

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -14,6 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 const fs_1 = __importDefault(require("fs"));
+const util_1 = require("./util");
 class CsvFileReader {
     constructor(fileName) {
         this.fileName = fileName;
@@ -27,7 +28,17 @@ class CsvFileReader {
                 .map((row) => {
                 return row.split(",");
             })
-                .map(this.mapRow);
+                .map((row) => {
+                return [
+                    (0, util_1.dateStringToDate)(row[0]), //date
+                    row[1],
+                    row[2],
+                    parseInt(row[3]),
+                    parseInt(row[4]),
+                    row[5],
+                    row[6],
+                ];
+            });
         });
     }
 }
--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,11 @@
 import fs from "fs";
+import { dateStringToDate } from "./util";
+import { MatchResult } from "./MatchResult";
 
-
-
-
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+export class CsvFileReader {
+  data: MatchData[] = [];
   constructor(public fileName: string) {}
-  abstract mapRow(row: string[]): T;
   async read(): Promise<void> {
     this.data = fs
       .readFileSync(this.fileName, { encoding: "utf-8" })
@@ -14,6 +13,16 @@ export abstract class CsvFileReader<T> {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map(this.mapRow);
+      .map((row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]), //date
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6],
+        ];
+      });
   }
 }
