e8afe2ef628007986cf7feb182aab58e9ea51144
Bikal <bikaladhikari5@gmail.com>
2024-06-30 16:09:48 +1000

    Using composition to refactor code

--- build/index.js
+++ build/index.js
@@ -1,13 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
+const CsvFileReader_1 = require("./CsvFileReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new MatchReader_1.MatchReader("football.csv");
-reader.read();
-const dateOfFirstMatch = reader.data[0];
+const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+const dateOfFirstMatch = matchReader.matches[0];
 console.log(dateOfFirstMatch[0]);
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
--- src/inheritance/MatchReader.ts
+++ src/inheritance/MatchReader.ts
@@ -1,6 +1,6 @@
 import { CsvFileReader } from "./CsvFileReader";
-import { MatchResult } from "./MatchResult";
-import { dateStringToDate } from "./util";
+import { MatchResult } from "../MatchResult";
+import { dateStringToDate } from "../util";
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 export class MatchReader extends CsvFileReader<MatchData> {
